0f3b5b340c10957bf8fd1d1645812fb90653c85a
Alcibíades Cabral Díaz <alcibiades@MacBook-Pro-de-Alcibiades.local>
2020-10-17 09:22:22 +0200

    axis update from all and change in the store local

--- src/stores/axisStore.js
+++ src/stores/axisStore.js
@@ -5,7 +5,7 @@ const STORE_PREFIX = 'axis_';
 
 const itemsData = localStorage.getItem(`${STORE_PREFIX}items`);
 
-export const axisStore = writable(JSON.parse(itemsData) || []);
+export const axisStore = writable(JSON.parse(itemsData) || "productOwner");
 
 axisStore.subscribe(value => {
     if (itemsData !== value) {
