6418aa5dd6fd3fa80b46911a5406eeb2b46bb620
Alcibíades Cabral Díaz <acabdia@gmail.com>
2020-10-20 15:33:39 +0200

    fix colors  change

--- src/components/Moderar.svelte
+++ src/components/Moderar.svelte
@@ -3,14 +3,80 @@
 	export let key;
 	export let item;
 	export let i;
+	import { axisStore } from "./../stores/axisStore.js";
+	import {
+		productOwner,
+		scrumMaster,
+		rte,
+		tribeLead,
+		ttl,
+	} from "../AxisBBDD.js";
 
 	const isPair = () => {
-		let result = Number(key.substring(4, 5)) % 2 == 0 ? true : false;
+		let axis = key.substring(0, 1);
+		let level = Number(key.substring(2, 3));
+		let group = Number(key.substring(4, 5));
+
+		let axisToNumber = () => {
+			switch (axis) {
+				case "A":
+					return 1;
+					break;
+
+				case "B":
+					return 2;
+					break;
+
+				case "C":
+					return 3;
+					break;
+
+				case "D":
+					return 4;
+					break;
+
+				default:
+					break;
+			}
+		};
+		let sumCodes = axisToNumber() + level + group;
+
+		let result = sumCodes % 2 == 0 ? true : false;
 		console.log(result);
 		return result;
 	};
 
 	$: selectValue = 0;
+
+	// Tags
+
+	$: tag = [0, 0, 0, 0];
+
+	afterUpdate(() => {
+		switch ($axisStore) {
+			case "productOwner":
+				tag = productOwner;
+				break;
+			case "scrumMaster":
+				tag = scrumMaster;
+				break;
+
+			case "rte":
+				tag = rte;
+				break;
+
+			case "tribeLead":
+				tag = tribeLead;
+				break;
+
+			case "ttl":
+				tag = ttl;
+				break;
+
+			default:
+				break;
+		}
+	});
 </script>
 
 <style>
@@ -28,14 +94,8 @@
 			Puntuación:
 			{$moderateStore[i][key] ? $moderateStore[i][key] : 0}
 		</p>
-		<!-- 
-		<p class=" f6 tr mb1 fl w-50">
-			<input
-				type="number"
-				bind:value={$moderateStore[i][key]}
-				placeholder="Moderar" />
-		</p> -->
 
+		<h3>Hola</h3>
 		<p class=" f6 tr mb1 fl w-50">
 			Moderar:
 			<!-- svelte-ignore a11y-no-onchange -->
