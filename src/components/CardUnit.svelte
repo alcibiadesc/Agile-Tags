777411ef2d9f9cf3db6e968619ece82c761a98bc
Alcibíades Cabral Díaz <acabdia@gmail.com>
2020-10-21 07:55:49 +0200

    change table result and moderator order

--- src/components/CardUnit.svelte
+++ src/components/CardUnit.svelte
@@ -443,6 +443,20 @@
             </p>
         </div>
     </div>
+		<div class:invisible={showChart}>
+    <TableResult
+        {axisA}
+        {axisB}
+        {axisC}
+        {axisD}
+        {participantAll}
+        {practitionerAll}
+        {expertAll}
+        {allAxisA}
+        {allAxisB}
+        {allAxisC}
+        {allAxisD} />
+</div>
     <div class="tl " class:invisible={hideModerator}>
         <slot />
 
@@ -462,18 +476,4 @@
                     d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z" /></svg>
         </div>
     </div>
-</div>
-<div class:invisible={showChart}>
-    <TableResult
-        {axisA}
-        {axisB}
-        {axisC}
-        {axisD}
-        {participantAll}
-        {practitionerAll}
-        {expertAll}
-        {allAxisA}
-        {allAxisB}
-        {allAxisC}
-        {allAxisD} />
 </div>
