4a38ce2fc7f16aa06c156ca4d6994193ca897176
Alcibíades Cabral Díaz <acabdia@gmail.com>
2020-12-03 13:36:00 +0100

    added

--- src/components/organisms/Cards.svelte
+++ src/components/organisms/Cards.svelte
@@ -5,6 +5,7 @@
 	import Moderar from "./../molecules/Moderar.svelte";
     let listItems = $items || [];
 
+
     const removeAccents = (str) => {
         return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     };
